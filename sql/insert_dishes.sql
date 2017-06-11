e27ebd9aeafd9a04bbb19e5e54e24b40c8d6da35
Nicola <buretta.nicola@gmail.com>
2017-06-11 17:15:34 +0200

    sql insert

--- sql/insert_dishes.sql
+++ sql/insert_dishes.sql
@@ -1,6 +1,6 @@
-insert into pos.dishes(name, price, id_type) values('Margherita', 4.5, 1);
-insert into pos.dishes(name, price, id_type) values('Patatosa', 5, 1);
-insert into pos.dishes(name, price, id_type) values('Prosciutto funghi', 5.5, 1);
-insert into pos.dishes(name, price, id_type) values('Patatine fritte', 2, 4);
-insert into pos.dishes(name, price, id_type) values('Hamburger vegetariano', 7, 3);
-insert into pos.dishes(name, price, id_type) values('Prosciutto e formaggio', 4.5, 2);
+insert into pos.dishes(name, price, id_type) values('Margherita', 4.5, 5);
+insert into pos.dishes(name, price, id_type) values('Patatosa', 5, 5);
+insert into pos.dishes(name, price, id_type) values('Prosciutto funghi', 5.5, 5);
+insert into pos.dishes(name, price, id_type) values('Patatine fritte', 2, 8);
+insert into pos.dishes(name, price, id_type) values('Hamburger vegetariano', 7, 7);
+insert into pos.dishes(name, price, id_type) values('Prosciutto e formaggio', 4.5, 6);
