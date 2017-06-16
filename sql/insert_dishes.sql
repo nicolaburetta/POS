f24b075d7968335846d9914139f1c7e92797ebf1
Nicola <buretta.nicola@gmail.com>
2017-06-17 00:36:29 +0200

    insert dishes types sql

--- sql/insert_dishes.sql
+++ sql/insert_dishes.sql
@@ -1,6 +1,6 @@
 insert into pos.dishes(name, price, id_type) values('Margherita', 4.5, 5);
 insert into pos.dishes(name, price, id_type) values('Patatosa', 5, 5);
 insert into pos.dishes(name, price, id_type) values('Prosciutto funghi', 5.5, 5);
-insert into pos.dishes(name, price, id_type) values('Patatine fritte', 2, 8);
-insert into pos.dishes(name, price, id_type) values('Hamburger vegetariano', 7, 7);
+insert into pos.dishes(name, price, id_type) values('Patatine fritte', 2, 7);
+insert into pos.dishes(name, price, id_type) values('Hamburger vegetariano', 7, 6);
 insert into pos.dishes(name, price, id_type) values('Prosciutto e formaggio', 4.5, 6);
